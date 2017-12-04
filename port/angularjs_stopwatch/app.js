3001f7acb25026cc13d283b7e59a35070fbab148
Alex P <alexp@itsokayitsofficial.io>
2017-12-04 07:20:01 -0700

    Again with the stopwatch

--- port/angularjs_stopwatch/app.js
+++ port/angularjs_stopwatch/app.js
@@ -8,7 +8,7 @@
  * URL: www.itsokayitsofficial.io
  */
 
-var app = angular.module('stopWatchApp', [])
+var stopWatchApp = angular.module('stopWatchApp', [])
 
   .controller('stopWatchDemoCtrl', ['$scope', function ($scope) {
     $scope.stopwatches = [{
