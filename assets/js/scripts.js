ebcaf0d1e2b9382f3d5e0a95781634b88707e6e9
ItsOkayItsOfficial <alexpugh@gmail.com>
2020-02-10 19:57:58 -0700

    Basic JS sudo

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+// Variable assignment
+
+// Drawing function to generate image
+
+// Calcultor function to generate new coords
+
+// Recursive function to repeat Calculator and Drawing function
+
+// Execute
